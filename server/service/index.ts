026a03a68b79006efe96778f9607c8f228ef9040
devdbystigga <walker.ryan182@gmail.com>
2021-03-24 18:01:12 -0500

    changed db host

--- server/service/index.ts
+++ server/service/index.ts
@@ -4,6 +4,6 @@ export const connectionPool: Pool = new Pool({
   user: "postgres",
   password: "rmw12612",
   database: "merkle_assessment",
-  host: "localhost",
+  host: "database-2.cgjxl6ure4bj.us-east-2.rds.amazonaws.com",
   port: 5432,
 });
