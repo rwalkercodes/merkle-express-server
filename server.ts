74bc02da0d90b2dfe555851279de5ae3aad0ef74
devdbystigga <walker.ryan182@gmail.com>
2021-03-26 12:17:17 -0500

    trying this

--- server.ts
+++ server.ts
@@ -8,7 +8,8 @@ const app = express();
 // cors config
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
