dfbfe82cdf740aa0fcf7d8e6e2cb60c2ecd8369a
devdbystigga <walker.ryan182@gmail.com>
2021-03-26 12:21:09 -0500

    CORS CONFIG

--- server.ts
+++ server.ts
@@ -6,16 +6,16 @@ import cors = require("cors");
 const app = express();
 
 // cors config
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: "GET,POST,OPTIONS",
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
+  })
+);
+
+app.use(express.json());
 
 // home route for AWS EB
 app.get("/", function (req, res) {
